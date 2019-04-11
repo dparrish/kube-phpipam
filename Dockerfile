07d040ced5b8f1599df3a0c114cb21dc50f6b118
David Parrish <david@dparrish.com>
2019-04-12 08:12:16 +1000

    Build with golang 1.12.3

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.11.6 as builder
+FROM golang:1.12.3 as builder
 
 WORKDIR /go/src
 ENV GO111MODULE=on
--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
 VERSION=`head -1 CHANGELOG | cut -d' ' -f2`
 echo "Building version $VERSION"
 
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker build -t dparrish/kube-phpipam:$VERSION .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push dparrish/kube-phpipam:$VERSION 
 
